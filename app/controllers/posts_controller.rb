5948d71d9d0433c6a7e0a448cea2237be3572014
afreen shaik <affushaik9493@gmail.com>
2023-10-05 16:56:10 +0530

    solve the linter issues

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,13 @@
 class PostsController < ApplicationController
-    def index
-        @user = User.find(params[:user_id])
-        @posts = @user.posts
-      end
+  def index
+    @user = User.find(params[:user_id])
+    @posts = @user.posts
+  end
 
-      def show
-        logger.debug(params)
-        @user = User.find(params[:user_id])
-        @post = @user.posts.find(params[:id])
-        @comments = @post.comments
-      end
+  def show
+    logger.debug(params)
+    @user = User.find(params[:user_id])
+    @post = @user.posts.find(params[:id])
+    @comments = @post.comments
+  end
 end
