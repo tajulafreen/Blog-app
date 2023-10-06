f12ed1624c6b806b24489e7379309b89f1e83e6c
afreen shaik <affushaik9493@gmail.com>
2023-10-07 00:44:18 +0530

    create current user method and create forms

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,4 +10,25 @@ class PostsController < ApplicationController
     @post = @user.posts.find(params[:id])
     @comments = @post.comments
   end
+
+  def new
+    @user = current_user
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      redirect_to user_posts_path, notice: 'Post was successfully created.'
+    else
+      flash.now[:alert] = 'Something Wrong, Cannot create a new post'
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text, :user_id)
+  end
 end
