da206f7a8694ce83b09e0d61d71cfeddaad17226
afreen shaik <affushaik9493@gmail.com>
2023-10-12 02:11:40 +0530

    add devise for sign up user

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,21 @@
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
   root "users#index"
+  # resources :users, only: [:index, :show] do
+  #   resources :posts do
+  #     resources :comments,  only: [:new, :create]
+  #     resources :likes,  only: [:create] 
+  #   end
+  # end
   resources :users, only: [:index, :show] do
-    resources :posts do
-      resources :comments,  only: [:new, :create]
-      resources :likes,  only: [:create] 
-    end
+    resources :posts, only: [:index, :new, :create, :show]
+  end
+  resources :posts do
+    resources :comments, only: [:new, :create]
+    resources :likes, only: [:create]
   end
 end
